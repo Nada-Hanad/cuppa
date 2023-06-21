3e5cb2d28d9999249323172ac82faebc5acff18b
Nada Hanad <jn_hanad@esi.dz>
2023-06-21 12:04:13 +0100

    fix: loading

--- pages/decideur/profil.jsx
+++ pages/decideur/profil.jsx
@@ -4,6 +4,7 @@ import { API_URL } from "../../config/api";
 import Head from "next/head";
 import Title from "../../components/shared/layout/title";
 import Image from "next/image";
+import { CircularProgress } from "@mui/material";
 
 const InfoLine = ({ title, value }) => {
   return (
@@ -15,11 +16,13 @@ const InfoLine = ({ title, value }) => {
 };
 
 export default function Profil() {
+  const [isLoading, setIsLoading] = useState(true);
+
   const [profil, setProfile] = useState({});
   const [openEdit, setOpenEdit] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
@@ -36,17 +39,19 @@ export default function Profil() {
     const response = await axios
       .put(
         `${API_URL}/api/account.management/modifyAccount/${profil?.role?.libelle_role}/${profil?.id_utilisateur}`,
-        data ,config
+        data,
+        config
       )
       .catch((e) => console.log(e));
     if (response) {
-     
       localStorage.setItem("token", token);
       setIsLoading(false);
       fetchProfile();
     }
   };
   const fetchProfile = async () => {
+    setLoading(true);
+
     const token = localStorage.getItem("token");
     console.log(token);
 
@@ -55,7 +60,10 @@ export default function Profil() {
     };
     const response = await axios
       .get(`${API_URL}/api/account.management/getProfileWithClient/`, config)
-      .catch((e) => console.log(e));
+      .catch((e) => console.log(e))
+      .finally(() => {
+        setIsLoading(false);
+      });
     if (response) {
       const profil = response.data;
       console.log(profil.data);
@@ -77,43 +85,45 @@ export default function Profil() {
         <Title title="Profil" />
       </div>
       <div className="flex flex-col items-center justify-center md:flex-row gap-x-20 ">
-        <div className="flex flex-col items-start justify-start px-8 pt-8 mx-auto mt-8 mb-24 text-center shadow-lg bg-slate-50 sm:w-full md:2/3 lg:w-1/3 min-h-96 ">
-          <div className="w-full mb-12">
-            <img
-              alt="profile"
-              height={48}
-              width={48}
-              className="object-cover w-48 h-48 mx-auto rounded "
-              src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
-            />
+        {isLoading ? (
+          <div className="flex h-[500px] w-96  items-center flex-col  justify-center pt-8 px-8 mx-auto mt-8 mb-24 text-center shadow-lg bg-slate-50 sm:w-full md:2/3 lg:w-1/3 min-h-96 ">
+            <CircularProgress />
           </div>
+        ) : (
+          <div className="flex flex-col items-start justify-start pt-8 px-8 mx-auto mt-8 mb-24 text-center shadow-lg bg-slate-50 sm:w-full md:2/3 lg:w-1/3 min-h-96 ">
+            <div className="w-full mb-12 ">
+              <img
+                alt="profile"
+                height={48}
+                width={48}
+                className="object-cover w-48 h-48 mx-auto rounded"
+                src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+              />
+            </div>
 
-          <InfoLine
-            title="Nom d 'utilisateur   "
-            value={profil?.username_utilisateur}
-          />
-          <InfoLine title="Role   " value={profil?.role?.libelle_role} />
-          <InfoLine title="Nom   " value={profil?.profil?.nom_utilisateur} />
-          <InfoLine
-            title="Prénom   "
-            value={profil?.profil?.prenom_utilisateur}
-          />
-          <InfoLine
-            title="Sexe   "
-            value={
-              profil?.profil?.sexe_utilisateur == "M"
-                ? "Homme"
-                : profil?.profil?.sexe_utilisateur == "F"
-                ? "Femme"
-                : ""
-            }
-          />
-          <InfoLine title="Client" value={profil?.client?.nom_client} />
-          <InfoLine
-            title="Superviseur  "
-            value={profil?.supervisor?.profil?.nom_utilisateur}
-          />
-        </div>
+            <InfoLine
+              title="Nom d 'utilisateur   "
+              value={profil?.username_utilisateur}
+            />
+            <InfoLine title="Role   " value={profil?.role?.libelle_role} />
+            <InfoLine title="Nom   " value={profil?.profil?.nom_utilisateur} />
+            <InfoLine
+              title="Prénom   "
+              value={profil?.profil?.prenom_utilisateur}
+            />
+            <InfoLine
+              title="Sexe   "
+              value={
+                profil?.profil?.sexe_utilisateur == "M"
+                  ? "Homme"
+                  : profil?.profil?.sexe_utilisateur == "F"
+                  ? "Femme"
+                  : ""
+              }
+            />
+            <InfoLine title="Client" value={profil?.client?.nom_client} />
+          </div>
+        )}
 
         <div className="flex flex-col items-center justify-center w-4/12">
           <button
